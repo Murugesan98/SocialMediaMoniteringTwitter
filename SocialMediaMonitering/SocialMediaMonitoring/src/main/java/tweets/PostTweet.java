776da24392e7ed60166acc0854feac117ef688e0
Murugesan98 <47205061+Murugesan98@users.noreply.github.com>
2021-02-24 21:38:33 +0530

    Key

--- SocialMediaMonitering/SocialMediaMonitoring/src/main/java/tweets/PostTweet.java
+++ SocialMediaMonitering/SocialMediaMonitoring/src/main/java/tweets/PostTweet.java
@@ -13,10 +13,10 @@ public class PostTweet
 	{
 		    ConfigurationBuilder cb = new ConfigurationBuilder();
 		    cb.setDebugEnabled(true)
-		    .setOAuthConsumerKey("wadldYLHBG8UsJZLpa31Uw")
-		    .setOAuthConsumerSecret("KOzAq53VF8bQ34OkRzDz3YhytnveQurpz4UCESgjddU6CN94")
-		    .setOAuthAccessToken("732546452187021312-3nmkB9Uzryqyl5RyKujyfNPJTF81u")
-		    .setOAuthAccessTokenSecret("kIaqRlGdgypDm10DDQegbWYpvb7ZwL2CFSXlxr8OzXJ");
+		    .setOAuthConsumerKey("w")
+		    .setOAuthConsumerSecret("K")
+		    .setOAuthAccessToken("73")
+		    .setOAuthAccessTokenSecret("k");
 		    Twitter twitter = new TwitterFactory(cb.build()).getInstance();
 		    JourneyLink attachment = new JourneyLink(); 
 		    if(result.equals("Positive"))
